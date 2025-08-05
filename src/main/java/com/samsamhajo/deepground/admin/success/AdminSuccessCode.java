80bc38bb339917fa68825de971fdd4d1a9b9b913
seanjung0915@gmail.com <seanjung0915@gmail.com>
2025-08-05 11:53:56 +0900

    feat: 관리자 대시보드 기능 관련 Controller 작성

---
+++ src/main/java/com/samsamhajo/deepground/admin/success/AdminSuccessCode.java
@@ -0,0 +1,26 @@
+package com.samsamhajo.deepground.auth.success;
+
+import com.samsamhajo.deepground.global.success.SuccessCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor
+public enum AdminSuccessCode implements SuccessCode {
+    // 대시보드 조회 성공
+    GET_DASHBOARD_SUCCESS(HttpStatus.OK, "대시보드 조회 성공");
+
+    private final HttpStatus httpStatus;
+    private final String message;
+
+    @Override
+    public HttpStatus getStatus() {
+        return httpStatus;
+    }
+
+    @Override
+    public String getMessage() {
+        return "[ADMIN] " + message;
+    }
+}
